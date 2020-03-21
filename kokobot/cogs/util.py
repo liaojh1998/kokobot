05c4d2eedee1d5a016468aec172fea2992f5fa73
Jie Hao Liao <liaojh1998@gmail.com>
2020-03-20 23:10:51 -0500

    Added shutdown utility

--- kokobot/cogs/util.py
+++ kokobot/cogs/util.py
@@ -21,3 +21,25 @@ class Util(commands.Cog):
         start = ctx.message.created_at.timestamp()
         end = datetime.datetime.utcnow().timestamp()
         await ctx.send('`{} ms`'.format(int((end - start) * 1000)))
+
+    @commands.command()
+    async def shutdown(self, ctx):
+        # admins
+        admin_role = ['bot boi', 'admin uwu', 'Officers']
+
+        can_shutdown = await ctx.bot.is_owner(ctx.message.author)
+        if not can_shutdown:
+            for role in ctx.channel.guild.roles:
+                if role.name in admin_role:
+                    if ctx.message.author in role.members:
+                        can_shutdown = True
+                        break;
+        # Shutdown
+        if can_shutdown:
+            sent = await ctx.send('Shutting down server...')
+            logger.info('Shutting down as {}'.format(ctx.message.author))
+            await ctx.bot.logout()
+        else:
+            sent = await ctx.send('Cannot shutdown as {}'.format(ctx.message.author))
+            await ctx.message.delete(delay=5)
+            await sent.delete(delay=5)
