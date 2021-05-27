45d02d6231e37833f69131f1e70090aecea1532b
Ubuntu <ubuntu@ip-172-31-34-60.us-east-2.compute.internal>
2021-05-27 04:05:49 +0000

    Modified roles to include gender roles

--- kokobot/cogs/roles.py
+++ kokobot/cogs/roles.py
@@ -10,13 +10,20 @@ emoji_bank = {
     ':hatching_chick:': '\U0001F423',
     ':zany_face:': '\U0001F92A',
     ':innocent:': '\U0001F607',
+
+    ':male_sign:': '\U00002642',
+    ':female_sign:': '\U00002640',
+    ':transgender_symbol:': '\U000026A7',
+    ':rainbow_flag:': '\U0001F3F3\uFE0F\u200D\U0001F308',
+
+    ':metal:': '\U0001F918',
     ':star2:': '\U0001F31F',
+    ':mortar_board:': '\U0001F393',
 }
 
 
 class Roles(commands.Cog):
-    """Modifies the member roles in this server. Check out #roles channel.
-
+    """Modifies the member roles in this server. Check out #\U0001F440-roles channel.
 
     Commands:
       +number -- Add a role
@@ -26,11 +33,15 @@ class Roles(commands.Cog):
         self.config = {
             'channel': 'roles',
             'emoji_roles': {
-                "Members": emoji_bank[':star2:'],
-                # 'E-Fam': emoji_bank[':woozy_face:'],
-                # 'Gus\'s Chicken Coop Fam': emoji_bank[':hatching_chick:'],
-                # 'JK Fam': emoji_bank[':zany_face:'],
-                # 'Wholesome Fam': emoji_bank[':innocent:'],
+                "Member": emoji_bank[':metal:'],
+                "Alumni": emoji_bank[':mortar_board:'],
+                "External Being": emoji_bank[':star2:'],
+            },
+            'gender_roles': {
+                "He/Him": emoji_bank[':male_sign:'],
+                "She/Her": emoji_bank[':female_sign:'],
+                "They/Them": emoji_bank[':transgender_symbol:'],
+                "Ask My Gender": emoji_bank[':rainbow_flag:'],
             },
             'invalid_roles': [
                 '@everyone', 'kokobot', 'bot boi', 'AI', 'Ascended Admin',
@@ -39,10 +50,18 @@ class Roles(commands.Cog):
             ],
         }
         self.config['invalid_roles'].extend(self.config['emoji_roles'])
+        self.config['invalid_roles'].extend(self.config['gender_roles'])
+
+        # Role Messages
+        emoji_role_message = 'React if you are an:\n'
+        gender_role_message = ('\nUse these roles below to help others on the server to identify you.\n'
+                               'Feel free to contact the officers for any questions, comments, or concerns!\n')
 
         # Initialize
         self.bot = bot
-        self.emoji_roles_bot = EmojiRoles(bot, self.config)
+        self.hello_ids = []
+        self.emoji_roles_bot = EmojiRoles(bot, self.config, 'emoji_roles', emoji_role_message)
+        self.gender_roles_bot = EmojiRoles(bot, self.config, 'gender_roles', gender_role_message)
         self.text_roles_bot = TextRoles(bot, self.config)
         self.bot.add_cog(self.emoji_roles_bot)
         self.bot.add_cog(self.text_roles_bot)
@@ -64,9 +83,12 @@ class Roles(commands.Cog):
         # Clean channels
         for channel in self.channels:
             await channel.purge(limit=None)
+            hello_id = await channel.send('Hello! :smiling_face_with_3_hearts:')
+            self.hello_ids.append(hello_id)
 
         # Ready other role bots
         await self.emoji_roles_bot.on_ready(self.channels)
+        await self.gender_roles_bot.on_ready(self.channels)
         await self.text_roles_bot.on_ready(self.channels)
 
         # Clear chat
@@ -74,8 +96,11 @@ class Roles(commands.Cog):
 
     async def clear(self):
         while True:
-            await asyncio.sleep(10 * 60) # every 10 minutes
+            await asyncio.sleep(60 * 60) # every 60 minutes
             message_ids = []
+            for hello_id in self.hello_ids:
+                if not hello_id is None:
+                    message_ids.append(hello_id)
             for channel in self.emoji_roles_bot.channel_info:
                 if ('message' in self.emoji_roles_bot.channel_info[channel]
                         and not self.emoji_roles_bot.channel_info[channel]['message'] is None):
@@ -91,9 +116,11 @@ class Roles(commands.Cog):
 
 
 class EmojiRoles(commands.Cog):
-    def __init__(self, bot, config):
+    def __init__(self, bot, config, roles_name, head_message):
         self.bot = bot
         self.config = config
+        self.roles_name = roles_name
+        self.head_message = head_message
         self.channels = {}
         self.guild_to_channel = {}
         self.channel_info = {}
@@ -119,24 +146,22 @@ class EmojiRoles(commands.Cog):
         # Setup roles
         self.channel_info[channel.id]['roles'] = {}
         for role in channel.guild.roles:
-            for emoji_role, emoji in self.config['emoji_roles'].items():
-                if  emoji_role in role.name:
+            for emoji_role, emoji in self.config[self.roles_name].items():
+                if emoji_role in role.name:
                     self.channel_info[channel.id]['roles'][emoji] = role
                     break
 
         if self.channel_info[channel.id]['roles']:
             # Send help string
-            help_str = 'Hello! :smiling_face_with_3_hearts:\n'
-            # for emoji, role in self.channel_info[channel.id]['roles'].items():
-            #     help_str += '\t\t* {}\n'.format(role.name)
-            # help_str += '\n'
-            help_str += 'React to become a member, unreact to become stray:\n'
-            # help_str += 'React an emoji to join a family, unreact an emoji to leave a family:\n'
+            help_str = self.head_message
+            for emoji_role, emoji in self.config[self.roles_name].items():
+                help_str += f'{emoji} {emoji_role}\n'
             self.channel_info[channel.id]['message'] = await channel.send(help_str)
 
             # React to the message
-            for emoji in self.channel_info[channel.id]['roles']:
-                await self.channel_info[channel.id]['message'].add_reaction(emoji)
+            for emoji in self.config[self.roles_name].values():
+                if emoji in self.channel_info[channel.id]['roles']:
+                    await self.channel_info[channel.id]['message'].add_reaction(emoji)
 
     async def redo_roles_on_create(self, role):
         logger.info('Server created role "{}".'.format(role.name))
@@ -226,11 +251,10 @@ class TextRoles(commands.Cog):
 
         if self.channel_info[channel.id]['roles']:
             # Send help string
-            help_str = 'Roles :clown::\n'
+            help_str = '\nShow your interests and potentially unlock new text channels with these roles:\n'
             for idx, role in enumerate(self.channel_info[channel.id]['roles']):
                 help_str += '\t\t`{}`: {}\n'.format(idx, role.name)
-            help_str += '\n'
-            help_str += 'Use `+number` to add or `-number` to remove a role for yourself.\n'
+            help_str += 'Use `+number` to add or `-number` to remove a role.\n'
             help_str += 'For example, `+0` will give you the role "{}", and `-0` will remove that role for you.'.format(self.channel_info[channel.id]['roles'][0].name)
             self.channel_info[channel.id]['message'] = await channel.send(help_str)
 
